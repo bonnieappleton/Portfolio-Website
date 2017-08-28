41b6add72a208fc726e0b9e919db89e1d19e6e11
bonnieappleton <bemappleton@hotmail.com>
2017-08-28 12:17:56 +0100

    Correct google verification

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponse
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('home.urls')),
-    url(r'^googlea14532fb912s486\.html$', lambda r: HttpResponse("google-site-verification: google6430b1c1c5f4d50f.html", mimetype="text/plain")),
+    url(r'^google6430b1c1c5f4d50f\.html$', lambda r: HttpResponse("google-site-verification: google6430b1c1c5f4d50f.html", content_type="text/plain")),
 ]
