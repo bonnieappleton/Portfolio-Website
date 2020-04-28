08473900b470fb79e43b3c2c33d9fa2d65cfe02c
Bonnie Appleton <bemappleton@gmail.com>
2020-04-28 14:56:26 +0100

    Updated homepage and added blog capability
    Blog not ready to use yet - using wordpress for the moment because I need to build in more power to add images to blog posts

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^page-analyser/', include('page_analyser.urls')),
     url(r'^google6430b1c1c5f4d50f\.html$', lambda r: HttpResponse("google-site-verification: google6430b1c1c5f4d50f.html", content_type="text/plain")),
+    url(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
